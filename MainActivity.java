d60ada7739c5aac5065b51bc2c5a08f3d74bfe65
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2018-05-20 20:50:45 +0200

    MainActivity.java
    MainActivity with a database created in Android Studio

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kasia.sqliteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDB = new DatabaseHelper(this);
+    }
+}
