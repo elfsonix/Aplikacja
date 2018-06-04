4b39e9d83f04c6438bef66c3fb1572e5251e168a
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2018-06-04 13:09:34 +0200

    Kod z MapsActivity do przerobienia

--- the_last_before_giving _up3/app/src/main/java/com/example/kasia/the_last_before_giving_up/MapsActivity.java
+++ the_last_before_giving _up3/app/src/main/java/com/example/kasia/the_last_before_giving_up/MapsActivity.java
@@ -3,12 +3,19 @@ package com.example.kasia.the_last_before_giving_up;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,10 +44,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+//When Map Loads Successfully
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                //Your code where exception occurs goes here...
+                List<LatLng> locations = new ArrayList<>();
+                locations.add(new LatLng(50.0532,19.9559));
+                locations.add(new LatLng(50.0668,19.9456));
+                locations.add(new LatLng(50.0639,19.9999));
+                locations.add(new LatLng(50.0279,19.9506));
+                locations.add(new LatLng(50.0631,19.9399));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                for(LatLng latLng : locations) {
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Title can be anything"));
+                }
+
+
+                //LatLngBound will cover all your marker on Google Maps
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                builder.include(locations.get(0)); //Taking Point A (First LatLng)
+                builder.include(locations.get(locations.size() - 1)); //Taking Point B (Second LatLng)
+                LatLngBounds bounds = builder.build();
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 200);
+                mMap.moveCamera(cu);
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+                //dotąd wyświetla tylko punkty z info: 'title can be anything' - 1. wersja
+
+                //ten kod wyświetla punkt z info o nazwie i populacji -> przerobić locations.add(new LatLng...
+                //na poniższy fragment kodu dla każdego miejsca (potem trzeba usunąć te locations.add; nie można
+                //użyć tych samych współrzędnych dwa razy, bo wywala błędy
+                //w snippet chyba trzeba dodać te wszystkie inforamcje, tzn katergoaria, obniżka itd
+                LatLng BRISBANE = new LatLng(50.0562,19.9580);
+                Marker mBrisbane = mMap.addMarker(new MarkerOptions()
+                        .position(BRISBANE)
+                        .title("Brisbane")
+                        .snippet("Population: 2,074,200")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            }
+        });
+
     }
+
 }
