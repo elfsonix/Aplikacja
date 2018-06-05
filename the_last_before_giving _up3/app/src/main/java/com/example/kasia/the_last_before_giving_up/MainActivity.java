279802eeb4aa4ee829118044fd81e46c23c4bd64
tuuznik <39033831+tuuznik@users.noreply.github.com>
2018-06-05 10:03:47 +0200

    Dodanie mainactivity
    Stowrzenie strony startowej z przyciskiem, który przenosi do mapy

---
+++ the_last_before_giving _up3/app/src/main/java/com/example/kasia/the_last_before_giving_up/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.kasia.the_last_before_giving_up;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void click(View view) {
+        switch (view.getId()){
+            case R.id.firstbtn:
+                Intent intent= new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
